f5ec07624ad7313ca5c5b524b3fe2abc8d815c7c
Jeremy Dobbins-Bucklad <jad-b@users.noreply.github.com>
2024-01-12 10:06:25 -0500

    Read AssemblyType from ASSEMBLY_TYPE env var

--- tests/acceptance.rs
+++ tests/acceptance.rs
@@ -1,23 +1,3 @@
-use std::convert::TryFrom;
-use std::env;
-
-enum AssemblyType{
-    Domain,
-    Live,
-}
-
-impl TryFrom<&str> for AssemblyType {
-    type Error = String;
-
-    fn try_from(val: &str) -> Result<Self, Self::Error> {
-        match val.to_ascii_lowercase().as_str() {
-            "domain" => Ok(Self::Domain),
-            "live" => Ok(Self::Live),
-            other => Err(format!("'{other}' isn't a supported test assembly")),
-        }
-    }
-}
-
 #[test]
 fn gets_all_categories_with_targets() {
     let scenario = assembly::assembly().new_scenario();
@@ -31,14 +11,45 @@ fn gets_all_categories_with_targets() {
 }
 
 pub mod assembly {
-    use std::sync::OnceLock;
+    use std::{
+        convert::TryFrom,
+        env,
+        sync::OnceLock,
+    };
+
+    const ASSEMBLY_TYPE: &str = "ASSEMBLY_TYPE";
+
+    #[derive(Debug)]
+    enum AssemblyType{
+        Domain,
+        Live,
+    }
+
+    impl TryFrom<String> for AssemblyType {
+        type Error = String;
+
+        fn try_from(mut val: String) -> Result<Self, Self::Error> {
+            val.make_ascii_lowercase();
+            match val.as_str() {
+                "domain" => Ok(Self::Domain),
+                "live" => Ok(Self::Live),
+                other => Err(format!("'{other}' isn't a supported test assembly")),
+            }
+        }
+    }
 
     pub fn assembly() -> &'static Assembly {
         static ASSEMBLY : OnceLock<Assembly> = OnceLock::new();
-        ASSEMBLY.get_or_init(|| Assembly{})
+        let asm_type = AssemblyType::try_from(
+                env::var(ASSEMBLY_TYPE).unwrap_or("Domain".to_string()))
+            .unwrap();
+        print!("Assembly Type = {asm_type:?}");
+        ASSEMBLY.get_or_init(|| Assembly{asm_type})
     }
 
-    pub struct Assembly { }
+    pub struct Assembly {
+        asm_type: AssemblyType,
+    }
 
     impl Assembly {
         pub fn new_scenario(&self) -> Scenario {
